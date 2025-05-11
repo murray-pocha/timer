c1034a0b8d5758253611f7291c10e8a1b7f29278
murray-pocha <murray.pocha@gmail.com>
2025-05-11 17:55:06 -0600

    refactor, clean code

--- timer.js
+++ timer.js
@@ -1,21 +1,17 @@
-
-//get command line arguments (arg)
 const args = process.argv.slice(2);
-//loop through
-for (let i = 0; i < args.length; i++) {
-  //Will have to change the arguments to a number
-  const time = Number(args[i]);
-  //make sure time is a positive number
+
+args.forEach(arg => {
+  const time = Number(arg);
+
   if (!isNaN(time) && time > 0) {
-    //set a timeout for the sound
     setTimeout(() => {
-      process.stdout.write('\x07'); //out put the sound alarm
+      process.stdout.write('\x07'); // Beep sound
       console.log(`Alarm goes off in ${time} seconds`);
-    }, time * 1000); //second should be changed to millisecond
+    }, time * 1000);
   } else {
-    console.log(`Not a proper time input: ${args[i]}`); //I know its not needed but I like a prompt so I can see whats wrong.
+    console.log(`Invalid time input: "${arg}"`);
   }
-}
+});
 
 
 
